91fcaa3b2835e3ee465c68960a2d5c14c8a6d338
UnknownVisuals <reynaldig40@gmail.com>
2025-04-23 19:55:20 +0700

    added datasets & instruction

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+# import pandas as pd
+# from sklearn.model_selection import train_test_split
+# from sklearn.preprocessing import StandardScaler, LabelEncoder
+
+
 def main():
+
+    # data = pd.read_csv('datasets/Dataset1/Dataset1.csv')
     print("Hello from dl-midtermproject!")
 
 
